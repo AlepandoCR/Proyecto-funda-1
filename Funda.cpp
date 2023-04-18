21098bff515fc071e0bcf7e09e3957104256e34e
AlepandoCR <70236880+AlepandoCR@users.noreply.github.com>
2023-04-18 06:15:27 -0600

    Update Funda.cpp

--- Funda.cpp
+++ Funda.cpp
@@ -19,62 +19,95 @@ int acumulado(int acu,int i,bool salioacu)
 	return acu;
 }
 //Generar un numero random para elegir el premio
-int GenPremios(int premios[],int veces_disponibles[])
+int GenPremios(int acudis, int d, int dd, int ds, int ddd, int dos, int u, int acu)
 {
+	int premio,Nrandom;
 	srand(time(NULL));
-	int premio_elegido;
-	do {
-		premio_elegido = rand() % 7;
-	} while (veces_disponibles[premio_elegido] == 0);
-	veces_disponibles[premio_elegido]--;
-	return premios[premio_elegido];
+	while(true)
+	{
+		Nrandom = rand() % 51;
+		if(Nrandom <= 9 && d > 0)
+		{
+			d--;
+			premio = 2000000;
+			return premio;
+		}else if(Nrandom <= 19 && dd > 0)
+		{
+			dd--;
+			premio = 2500000;
+			return premio;
+		}else if(Nrandom <= 35 && ds > 0)
+		{
+			ds--;
+			premio = 3000000;
+			return premio;
+		}else if(Nrandom <= 45 && ddd > 0)
+		{
+			ddd--;
+			premio = 5000000;
+			return premio;
+		}else if(Nrandom <= 47 && dos > 0)
+		{
+			dos--;
+			premio = 10000000;
+			return premio;
+		}else if(Nrandom == 48 && u > 0)
+		{
+			u--;
+			premio = 15000000;
+			return premio;
+		}else if(Nrandom == 49 && acudis == 1)
+		{
+			acudis--;
+			premio = acu;
+			return premio;
+		}else if(Nrandom > 48 && Nrandom < 50 && acudis == 2)
+		{
+			acudis--;
+			premio = acu;
+			return premio;
+		}
+	}
 }
-
 int main()
 {
 	//Seed del generador random
 	srand(time(NULL));
-	int acu = 100000000, acudis = 1, d = 10, dd = 10, ds = 16, ddd = 10, dos = 2, u = 1,acudiv,cantidadDePremios = 49;
-	int Pelegidos[50];
-	int numeroG[50],numeroGExtra[60];
-	int serieG[50],serieGExtra[60];
-	bool salioacu = false,bolitaB = false,SorteoExtra = false;
+	int Acumulado = 100000000, acudis = 1, d = 10, dd = 10, ds = 16, ddd = 10, dos = 2, u = 1,acudiv,premioElegido,numeroG, numeroGExtra, serieG, serieGExtra;
+	bool salioAcu = false,bolitaB = false,SorteoExtra = false;
 	for (int i = 0; i <= 49; i++)
 	{
 		if(i == 49 && SorteoExtra)
 		{
 			break;
 		}
-		if(i >= 11 && !salioacu && !bolitaB)
+		if(i >= 11 && !salioAcu && !bolitaB)
 		{
 			system("CLS");
 			acudis++;
 			bolitaB = true;
-			cout << "Una nueva bolita de acumulado ha sido agregada!"<<endl;
-			Sleep(2000);
+			cout << "				Una nueva bolita de acumulado ha sido agregada!"<<endl;
+			Sleep(3000);
 			system("ClS");
-		}else if(i >= 20 && !salioacu && !SorteoExtra)
+		}else if(i >= 20 && !salioAcu && !SorteoExtra)
 		{
 			system("CLS");
 			acudis = 0;
 			SorteoExtra = true;
-			acudiv = acu / 60;
-			cantidadDePremios = 48;
-			cout << "El acumulado no salio, por lo tanto el premio se dividira en 60 partes iguales al final del sorteo";
+			acudiv = Acumulado / 60;
+			cout << "		El acumulado no salio, por lo tanto el premio se dividira en 60 partes iguales al final del sorteo";
 			Sleep(5000);
 			system("CLS");
 		}
 		
-		acu = acumulado(acu, i,salioacu);
-		int premios[] = { 2000000, 2500000, 3000000, 5000000, 10000000, 15000000,acu };
-		int veces_disponibles[] = { d, dd, ds, ddd, dos, u,acudis };
+		Acumulado = acumulado(Acumulado, i,salioAcu);
 		system("CLS");
 		//Sorteo de Numeros y Series
-		numeroG[i] = rand() % 100;
-		serieG[i] = rand() % 1000;
-		Pelegidos[i] = GenPremios(premios, veces_disponibles);
+		numeroG = rand() % 100;
+		serieG = rand() % 1000;
+		premioElegido = GenPremios(acudis,d,dd,ds,ddd,dos,u,Acumulado);
 		//Cada vez que salga un premio, disminuye la cantidad de veces que este esta disponible
-		switch (Pelegidos[i])
+		switch (premioElegido)
 		{
 		case 2000000:
 			d--;
@@ -97,79 +130,69 @@ int main()
 		default:
 			break;
 		}
-		if(Pelegidos[i] == acu)
+		if(premioElegido == Acumulado)
 		{
-			cout << "Salio el acumulado! con el valor de: " << acu<<endl;
+			cout << "				Salio el acumulado! con el valor de: " << Acumulado << endl;
 			Sleep(2000);
 			system("CLS");
-			salioacu = true;
+			salioAcu = true;
 			acudis = 0;
 		}
-		cout << "Tirada numero " << i + 1 << endl;
-		cout << "Ganador #" << i + 1 << ": " << numeroG[i] << " Con la serie: " << serieG[i]<<" y su premio: "<<Pelegidos[i] << endl;
+		cout << "		---------------------------------------------------------------------------------" << endl;
+		cout << "		|				    Tirada numero " << i + 1 << "				|" << endl;
+		cout << "		|		Ganador #" << i + 1 << ": " << numeroG << " Con la serie: " << serieG<<" y su premio: "<< premioElegido << "		|" << endl;
+		cout << "		---------------------------------------------------------------------------------" << endl;
 		cout << endl << endl;
-		if (!salioacu)
+		cout << "				-------------------------------------------------";
+		cout << endl << "				|   Premios	  	|   	     Restantes  |"<<endl;
+		cout << "				-------------------------------------------------"<<endl;
+		cout << "				|   2000000             |                " << d << "	|" << endl;
+		cout << "				-------------------------------------------------"<<endl;
+		cout << "				|   2500000             |                " << dd << "	|" << endl;
+		cout << "				-------------------------------------------------" << endl;
+		cout << "				|   3000000             |                " << ds << "	|" << endl;
+		cout << "				-------------------------------------------------" << endl;
+		cout << "				|   5000000             |                " << ddd << "	|" << endl;
+		cout << "				-------------------------------------------------" << endl;
+		cout << "				|   10000000            |                " << dos << "	|" << endl;
+		cout << "				-------------------------------------------------" << endl;
+		cout << "				|   15000000            |                " << u << "	|" << endl;
+		cout << "				-------------------------------------------------" << endl;
+		cout << "				|   Acumulado           |                " << acudis << "	|" << endl;
+		cout << "				-------------------------------------------------"<<endl<<endl;
+		if (!salioAcu && !SorteoExtra)
 		{
-			cout << "Valor del acumulado: " << acu << " de colones" << endl;
-		}
-		cout << endl << "Premios restantes:"<<endl;
-		for(int k = 0;k <= 6;k++)
-		{
-			
-			cout <<premios[k]<< ": " << veces_disponibles[k]<<endl;
+			cout << "	    -#-#-#-#-#-#-#-#-#-#-#- Valor del acumulado: " << Acumulado << " de Colones -#-#-#-#-#-#-#-#-#-#-#-" << endl << endl;
 		}
 		system("pause");
 		Sleep(1231.120201001);
 	}
-	Sleep(4000);
-	system("CLS");
-	cout << endl << "lista de ganadores:" << endl;
-	
-	for (int i = 0; i <= cantidadDePremios; i++)
-	{
-		if(Pelegidos[i] == acu)
-		{
-			cout << numeroG[i] << " serie: " << serieG[i] << " premio: " << "ACUMULADO! ("<<Pelegidos[i]<<")" << endl;
-		}
-		else {
-			cout << numeroG[i] << " serie: " << serieG[i] << " premio: " << Pelegidos[i] << endl;
-		}
-	}
+	Sleep(2000);
 	if (SorteoExtra)
 	{
-		cout << "Para iniciar con el sorteo"<<endl;
+		system("CLS");
+		cout << "Para iniciar con el sorteo extra"<<endl<<endl;
 		system("pause");
 		system("CLS");
-		cout << endl << endl << "Los siguientes numeros tienen premio extra de: " << acudiv << endl;
+		cout << endl << endl << "Los siguientes numeros tienen premio extra de: " << acudiv << "colones" << endl;
 		Sleep(3000);
 		for (int i = 0; i <= 59; i++)
 		{
 			system("CLS");
-			cout << "Sorteo Extra #" << i + 1;
-			numeroGExtra[i] = rand() % 100;
-			serieGExtra[i] = rand() % 1000;
-			cout << "Numero: " << numeroGExtra[i] << " Serie: " << serieGExtra[i]<<endl;
+			cout << "+---------------------------------------------------+\n";
+			cout << "|                   Sorteo Extra #" << i + 1 << "                  |\n";
+			cout << "|---------------------------------------------------|\n";
+			numeroGExtra = rand() % 100;
+			serieGExtra = rand() % 1000;
+			cout << "|  Numero: " << numeroGExtra << "  Serie: " << serieGExtra;
+			cout << "  Premio: " << acudiv << "         |\n";
+			cout << "+---------------------------------------------------+\n";
 			system("pause");
 			Sleep(1000);
 		}
-		cout << "Ganadores Principales"<<endl;
-		for (int i = 0; i <= cantidadDePremios; i++)
-		{
-			if (Pelegidos[i] == acu)
-			{
-				cout << numeroG[i] << " serie: " << serieG[i] << " premio: " << "ACUMULADO! (" << Pelegidos[i] << ")" << endl;
-			}
-			else {
-				cout << numeroG[i] << " serie: " << serieG[i] << " premio: " << Pelegidos[i] << endl;
-			}
-		}
-		cout << "Ganadores Extra:"<<endl;
-		for(int i = 0; i <= 59;i++)
-		{
-			cout << "Numero: " << numeroGExtra[i] << " Serie: " << serieGExtra[i]<<" Con premio de: "<<acudiv<<endl<<endl<<endl;
-		}
-		cout << endl << "Para terminar todos los sorteos:";
-		system("pause");
 	}
+	system("CLS");
+	cout << endl << endl << "Para terminar todos los sorteos:";
+	system("pause");
 	return 0;
 }
